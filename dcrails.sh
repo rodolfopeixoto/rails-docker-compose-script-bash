258bbe2fd7d9294a2f8285e7e4121a5d8d705b40
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2017-07-05 16:40:09 -0300

    Fixed error in variable myapp in docker-compose.yml

--- dcrails.sh
+++ dcrails.sh
@@ -12,10 +12,33 @@ touch Dockerfile
 echo "
 FROM ruby:2.3.3
 RUN apt-get update -qq && apt-get install -y build-essential libpq-dev nodejs
-RUN mkdir /myapp
-WORKDIR /myapp
-ADD Gemfile /myapp/Gemfile
-ADD Gemfile.lock /myapp/Gemfile.lock
+RUN mkdir /$myapp
+WORKDIR /$myapp
+ADD Gemfile /$myapp/Gemfile
+ADD Gemfile.lock /$myapp/Gemfile.lock
 RUN bundle install
-ADD . /myapp
-  " > Dockerfile
+ADD . $myapp
+" > Dockerfile
+
+touch Gemfile
+echo
+"
+source 'https://rubygems.org'
+gem 'rails', '5.0.0.1'
+" > Gemfile
+touch Gemfile.lock
+touch docker-compose.yml
+
+echo "version: '3'
+services:
+  db:
+    image: postgres
+  web:
+    build: .
+    command: bundle exec rails s -p 3000 -b '0.0.0.0'
+    volumes:
+      - .:/$myapp
+    ports:
+      - "3000:3000"
+    depends_on:
+      - db" > docker-compose.yml
