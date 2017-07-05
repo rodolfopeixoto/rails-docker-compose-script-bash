c8e0e4d3d651a57f5b933b81fd7b7caf24d175b7
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2017-07-05 17:55:57 -0300

    Created envrionement and create table of the database

--- dcrails.sh
+++ dcrails.sh
@@ -9,8 +9,7 @@ mkdir $myapp
 cd $myapp
 touch Dockerfile
 
-echo "
-FROM ruby:2.3.3
+echo "FROM ruby:2.3.3
 RUN apt-get update -qq && apt-get install -y build-essential libpq-dev nodejs
 RUN mkdir /$myapp
 WORKDIR /$myapp
@@ -18,15 +17,16 @@ ADD Gemfile /$myapp/Gemfile
 ADD Gemfile.lock /$myapp/Gemfile.lock
 RUN bundle install
 ADD . $myapp
+RUN bundle install
 " > Dockerfile
 
 touch Gemfile
-echo
-"
-source 'https://rubygems.org'
+echo "source 'https://rubygems.org'
 gem 'rails', '5.0.0.1'
 " > Gemfile
+
 touch Gemfile.lock
+
 touch docker-compose.yml
 
 echo "version: '3'
@@ -42,3 +42,35 @@ services:
       - "3000:3000"
     depends_on:
       - db" > docker-compose.yml
+
+docker-compose build
+docker-compose run web rails new . --force --database=postgresql
+sudo chown -R $USER:$USER .
+
+cd config
+rm -R database.yml
+touch database.yml
+
+echo "default: &default
+  adapter: postgresql
+  encoding: unicode
+  pool: 5
+  host: db
+  username: postgres
+  password:
+development:
+  <<: *default
+  database: ${myapp}_development
+test:
+  <<: *default
+  database: ${myapp}_test
+production:
+  <<: *default
+  database: ${myapp}_production
+  username: $myapp
+  password: <%= ENV['${myapp^^}_DATABASE_PASSWORD'] %>
+" > database.yml
+
+docker-compose build
+
+docker-compose run web rails db:create
